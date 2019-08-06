80d34cd4cbee1449a9fa4c7d9c2c89f9a34cb201
Olivia Chavez <oliviachavezux@gmail.com>
2019-08-06 08:57:48 -0400

    Homework 3
    Ready for grading

---
+++ starter_code/Intro to JS.js
@@ -0,0 +1,81 @@
+/*
+
+I'm a multi-line comment in javascript 
+
+1. Variables 
+2. Functions 
+3. Conditionals 
+4. Loops
+
+
+Pseudo-Code 
+
+discount_code = 0.55
+show discount_code
+discount_code*5
+
+1. variables give us two things: 
+
+- fewer errors
+- consistent values (also helps new team members)
+
+------------------
+
+2. Functions are self contained blocks of code that are re-useable  
+
+priceInCart = 25
+priceInCart * (1-discount_code)
+
+function findDiscount () {
+priceInCart * (1-discount_code)
+
+}
+-----------------
+
+3. Conditionals are a way to choose between different paths based on a condition.  
+
+If something, then something, otherwise
+if there are apples, then I buy apples. 
+Otherwise, I buy oranges. 
+
+If it's in stock <- condition (conditional)
+Then add to cart <- yes!
+Otherwise show a message to say "out of stock" <- no :(
+
+
+4. Looping is used to repeat a set of instructions over and over again. 
+
+For 10 times
+For every item in their cart 
+
+Problem: Write code for NestThermstat in the Winter
+
+getCurrentTemp
+getUserPeferredTemp
+
+1. What temp do we want?
+2. What temp is it right now? 
+
+getCurrentTemp // what is the room at right now 
+
+
+while thermostat is on
+preferredTemp = getUserPreferredTemp()
+currentTemp: getCurrentTemp
+
+IF preferredTemp > currentTemp
+THEN turn_on_heater
+OTHERWSISE turn_off_heater
+
+preferredTemp/currentTemp are variables 
+conditional for the IF
+
+
+-------
+You can get a specific element by typing it into the console. for an ID it will return a div
+
+
+
+*/
+
+
